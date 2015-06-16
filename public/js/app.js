57dcfefe2d2640d23e53ffb0c2ce3c0a16902d1e
James <jamespdlynn@gmail.com>
2015-06-15 22:05:13 -0400

    Added walk animations Added navigation animation Added angular js infrastructure

--- public/js/app.js
+++ public/js/app.js
@@ -1,112 +1,149 @@
-
- var app = angular.module("app",[]);
-
- app.controller('MainController', function($scope) {
-	 $scope.nav = {
-		 items : [
-			 {key:'about',path:'/#about-me'},
-			 {key:'portfolio',path:'/#portfolio'},
-			 {key:'contact', path:'/#contact'},
-			 {key:'resume', path:'/resume.pdf'}
-		 ],
-
-		 visible : false,
-
-		 toggle : function(){
-			 return (this.visible = !this.visible);
-		 }
-	 };
-
- }).directive('nav',function(){
-	 return {
-		 templateUrl : 'templates/nav.html',
-
-		 link : function(scope, element, attrs){
-			 /*scope.$watch('nav.visible', function(){
-			  angular.forEach(element.children(), function(item){
-			  var bubbles = item.children;
-			  var i = bubbles.length;
-			  while (i--){
-			  $animate.hide(bubbles[i]);
-			  }
-			  });
-			  });*/
-		 }
-	 }
- }).directive('avatar', function(){
-	 return{
-		 link: function (scope, element) {
-
-			 scope.reset = function(){
-				 element.removeClass('wave').removeClass('blink').removeClass('dance');
-			 };
-
-			 scope.blink = function(){
-				 scope.reset();
-				 element.addClass('blink');
-				 element[0].addEventListener( 'webkitAnimationEnd',  scope.reset);
-			 };
-
-			 scope.wave = function(){
-				 scope.reset();
-				 element.addClass('wave');
-				 element[0].addEventListener( 'webkitAnimationEnd',  scope.reset);
-			 };
-
-			 scope.dance = function(){
-				 scope.reset();
-				 element.addClass('dance');
-				 element[0].addEventListener( 'webkitAnimationEnd',  scope.reset);
-			 };
-
-			 scope.walk = function(){
-				 scope.reset();
-				 element.addClass('walk').toggleClass('left');
-				 element[0].addEventListener( 'webkitTransitionEnd', scope.reset);
-			 };
-
-			 scope.random = function(){
-				 var num = Math.random();
-				 if (num > 0.4){
-					 scope.blink();
-				 }else if (num > 0.1){
-					 scope.wave();
-				 }else{
-					 scope.dance();
-				 }
-			 }
-
-			 scope.onClick = function(){
-				 scope.wave();
-				 scope.nav.toggle();
-				 scope.$apply();
-			 };
+var app = angular.module("app", ['ngRoute']);
 
 
-			 setInterval(scope.random, 6000);
-		 }
-	 }
- });
+app.config(['$routeProvider',function($routeProvider) {
+	$routeProvider.
+		when('/about', {
+			state : 'about'
+		}).
+		when('/portfolio', {
+			state : 'portfolio'
+		}).
+		when('/contact', {
+			state : 'contact'
+		}).
+		otherwise({
+			state : ''
+		});
+}]);
 
+app.controller('MainController', function ($scope, $route, $location) {
 
-var $el = angular.element(document.querySelector('#avatar'));
+	$scope.nav = {
 
-function reset(){
-	var temp = angular.element('<div id="avatar"></div>');
-	$el.replaceWith(temp);
-	$el = temp;
-}
+		visible: false,
 
+		items: [
+			{key: 'about', path: '#/about', target:'_self'},
+			{key: 'portfolio', path: '#/portfolio', target:'_self'},
+			{key: 'contact', path: '#/contact', target:'_self'},
+			{key: 'resume', path: '/resume.pdf', target:'_blank'}
+		],
 
-window.Avatar = {
+		toggle: function () {
+			return (this.visible = !this.visible);
+		}
+	};
 
-	blink : function(){
-		reset();
-		$el.addClass('blink');
-	},
+	$scope.avatar = {
+		isLeft :  false,
 
-	walkLeft : function(){
-		reset();
-		$el.css('transform','scaleX(-1)').addClass('walk');
+		onClick : function(){
+
+			if (!this.isLeft){
+				$scope.nav.toggle();
+			} else{
+				$location.url('/');
+			}
+		}
 	}
-};
+
+	$scope.$on('$routeChangeSuccess', function() {
+		if ($route.current){
+
+			$scope.nav.visible = false;
+			$scope.avatar.isLeft = !!$route.current.state;
+			console.log($route.current.state+' '+$scope.avatar.isLeft);
+		}
+	});
+
+}).directive('jlNav', function () {
+	return {
+		restrict: 'E',
+
+		scope : true,
+
+		templateUrl : 'templates/nav.html'
+	}
+}).directive('jlAvatar', function () {
+
+	return {
+		restrict: 'E',
+
+		link: function (scope, element) {
+
+			var ANIMATIONS = ['wave', 'blink', 'dance', 'walk', 'turn'];
+			var ANIMATION_EVENTS = ['webkitAnimationEnd', 'oanimationend', 'msAnimationEnd', 'animationend'];
+			var TRANSITION_EVENTS = ['webkitTransitionEnd', 'otransitionend', 'oTransitionEnd', 'msTransitionEnd', 'transitionend'];
+
+			var reset = function () {
+				ANIMATIONS.forEach(function (animation) {
+					element.removeClass(animation);
+				});
+				element.removeClass('flip').removeClass('reverse');
+			};
+
+			var animate = function(type, callback){
+				type = type.toLowerCase();
+				callback = callback || reset;
+
+				if (~ANIMATIONS.indexOf(type)) {
+					reset();
+					element.addClass(type);
+					element.one(ANIMATION_EVENTS.join(' '), callback);
+				}
+				else {
+					console.error(type + ' is not a valid animation');
+				}
+			}
+
+			var togglePosition = function () {
+				reset();
+
+				if(!element.hasClass('left')){
+					element.addClass('flip');
+				}
+
+				element.addClass('turn');
+
+				element.one(ANIMATION_EVENTS.join(' '), function(){
+					element.removeClass('turn');
+					element.addClass('walk').toggleClass('left');
+
+					element.one(TRANSITION_EVENTS.join(' '), function(){
+						element.removeClass('walk');
+						element.addClass('turn reverse');
+
+						element.one(ANIMATION_EVENTS.join(' '),reset);
+					});
+				});
+			};
+
+			setInterval(function(){
+				var num = Math.random();
+				var type = (num >= 0.25 ? 'blink' : (scope >= 0.125 ? 'wave' : 'dance'));
+				if (!element.hasClass('walk') && !element.hasClass('turn')){
+					animate(type);
+
+				}
+			}, 7000);
+
+			window.avatar = {
+				blink : animate.bind(this,'blink'),
+				wave : animate.bind(this,'dance'),
+				dance : animate.bind(this,'dance')
+			};
+
+			scope.$watch('avatar.isLeft', function(value){
+				console.log(scope.avatar.isLeft);
+				if (scope.avatar.isLeft != element.hasClass('left')){
+					togglePosition();
+				}
+			});
+
+		}
+	}
+});
+
+
+
