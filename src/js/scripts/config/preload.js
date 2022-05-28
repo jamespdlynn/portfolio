a0d6f113794571779b80d1dd824d2f0084f1706e
James <jamespdlynn@gmail.com>
2022-05-28 17:14:08 -0500

    new images and copy for portfolio views

--- src/js/scripts/config/preload.js
+++ src/js/scripts/config/preload.js
@@ -5,167 +5,184 @@
  * @author James Lynn
  */
 define(function (require, exports, module) {
-	'use strict';
+  'use strict';
 
-	var assetsPath = module.config().path || '/assets';
+  var assetsPath = module.config().path || '/assets';
 
-	/**
-	 * @typedef {Object} asset
-	 * @property {string} id - Unique identifier of the asset
-	 * @property {string} type - The type of asset (image, audio, etc)
-	 * @property {string} src - The path to this asset from the server root @type string
-	 * @property {string} group - Identifier of the batch group this asset belongs to (necessary for loading and unloading in clumps)
-	 * @property {boolean} [cache=false] - Flag determining whether to store this object in memory after loading
-	 */
+  /**
+   * @typedef {Object} asset
+   * @property {string} id - Unique identifier of the asset
+   * @property {string} type - The type of asset (image, audio, etc)
+   * @property {string} src - The path to this asset from the server root @type string
+   * @property {string} group - Identifier of the batch group this asset belongs to (necessary for loading and unloading in clumps)
+   * @property {boolean} [cache=false] - Flag determining whether to store this object in memory after loading
+   */
 
-	/**@return asset[] */
-	return [
+  /**@return asset[] */
+  return [
 
-		{
-			id: 'sprites',
-			type: 'image',
-			src: assetsPath + '/img/sprites.png',
-			group: 'main'
-		},
+    {
+      id: 'sprites',
+      type: 'image',
+      src: assetsPath + '/img/sprites.png',
+      group: 'main'
+    },
 
-		{
-			id: 'bubble',
-			type: 'image',
-			src: assetsPath + '/img/bubble.png',
-			group: 'main'
-		},
+    {
+      id: 'bubble',
+      type: 'image',
+      src: assetsPath + '/img/bubble.png',
+      group: 'main'
+    },
 
-		{
-			id: 'click',
-			type: 'audio',
-			src: assetsPath + '/audio/click.mp3',
-			group: 'main',
-			cache: true
-		},
+    {
+      id: 'click',
+      type: 'audio',
+      src: assetsPath + '/audio/click.mp3',
+      group: 'main',
+      cache: true
+    },
 
-		{
-			id: 'bloop',
-			type: 'audio',
-			src: assetsPath + '/audio/bloop.mp3',
-			group: 'main',
-			cache: true
-		},
+    {
+      id: 'bloop',
+      type: 'audio',
+      src: assetsPath + '/audio/bloop.mp3',
+      group: 'main',
+      cache: true
+    },
 
-		{
-			id: 'swoosh',
-			type: 'audio',
-			src: assetsPath + '/audio/swoosh.mp3',
-			group: 'main',
-			cache: true
-		},
+    {
+      id: 'swoosh',
+      type: 'audio',
+      src: assetsPath + '/audio/swoosh.mp3',
+      group: 'main',
+      cache: true
+    },
 
-		{
-			id: 'walk',
-			type: 'audio',
-			src: assetsPath + '/audio/walk.mp3',
-			group: 'main',
-			cache: true
-		},
+    {
+      id: 'walk',
+      type: 'audio',
+      src: assetsPath + '/audio/walk.mp3',
+      group: 'main',
+      cache: true
+    },
 
-		{
-			id: 'scream',
-			type: 'audio',
-			src: assetsPath + '/audio/scream.mp3',
-			group: 'main',
-			cache: true
-		},
+    {
+      id: 'scream',
+      type: 'audio',
+      src: assetsPath + '/audio/scream.mp3',
+      group: 'main',
+      cache: true
+    },
 
-		{
-			id: 'portrait',
-			type: 'image',
-			src: assetsPath + '/img/portrait.png',
-			group: 'about',
-			cache: true
-		},
+    {
+      id: 'portrait',
+      type: 'image',
+      src: assetsPath + '/img/portrait.png',
+      group: 'about',
+      cache: true
+    },
 
-		{
-			id: 'PortfolioController',
-			type: 'module',
-			src: 'controller/port',
-			group: 'portfolio'
-		},
+    {
+      id: 'PortfolioController',
+      type: 'module',
+      src: 'controller/port',
+      group: 'portfolio'
+    },
 
-		{
-			id: '512Games',
-			type: 'image',
-			src: assetsPath + '/img/portfolio/512games.png',
-			group: 'portfolio',
-			cache: true
-		},
+    {
+      id: '512Games',
+      type: 'image',
+      src: assetsPath + '/img/portfolio/512games.png',
+      group: 'portfolio',
+      cache: true
+    },
 
-		{
-			id: '512GamesNav',
-			type: 'image',
-			src: assetsPath + '/img/portfolio/512games-nav.png',
-			group: 'portfolio',
-			cache: true
-		},
+    {
+      id: '512GamesNav',
+      type: 'image',
+      src: assetsPath + '/img/portfolio/512games-nav.png',
+      group: 'portfolio',
+      cache: true
+    },
 
-		{
-			id: 'wordJumble',
-			type: 'image',
-			src: assetsPath + '/img/portfolio/word-jumble.png',
-			group: 'portfolio',
-			cache: true
-		},
+    {
+      id: 'wordJumble',
+      type: 'image',
+      src: assetsPath + '/img/portfolio/word-jumble.png',
+      group: 'portfolio',
+      cache: true
+    },
 
-		{
-			id: 'wordJumbleNav',
-			type: 'image',
-			src: assetsPath + '/img/portfolio/word-jumble-nav.png',
-			group: 'portfolio',
-			cache: true
-		},
+    {
+      id: 'wordJumbleNav',
+      type: 'image',
+      src: assetsPath + '/img/portfolio/word-jumble-nav.png',
+      group: 'portfolio',
+      cache: true
+    },
 
-		{
-			id: 'aws',
-			type: 'image',
-			src: assetsPath + '/img/portfolio/aws.png',
-			group: 'portfolio',
-			cache: true
-		},
+    {
+      id: 'appStoreButton',
+      type: 'image',
+      src: assetsPath + '/img/portfolio/app-store.png',
+      group: 'portfolio',
+      cache: true
+    },
 
-		{
-			id: 'awsNav',
-			type: 'image',
-			src: assetsPath + '/img/portfolio/aws-nav.png',
-			group: 'portfolio',
-			cache: true
-		},
+    {
+      id: 'playStoreButton',
+      type: 'image',
+      src: assetsPath + '/img/portfolio/play-store.png',
+      group: 'portfolio',
+      cache: true
+    },
 
-		{
-			id: 'resume',
-			type: 'pdf',
-			src: assetsPath + '/docs/resume.pdf',
-			group: 'resume'
-		},
 
-		{
-			id: 'ContactController',
-			type: 'module',
-			src: 'controller/contact',
-			group: 'contact'
-		},
+    {
+      id: 'aws',
+      type: 'image',
+      src: assetsPath + '/img/portfolio/aws.png',
+      group: 'portfolio',
+      cache: true
+    },
 
-		{
-			id: 'check',
-			type: 'image',
-			src: assetsPath + '/img/check.png',
-			group: 'contact'
-		},
+    {
+      id: 'awsNav',
+      type: 'image',
+      src: assetsPath + '/img/portfolio/aws-nav.png',
+      group: 'portfolio',
+      cache: true
+    },
 
-		{
-			id: 'mail',
-			type: 'audio',
-			src: assetsPath + '/audio/mail.mp3',
-			group: 'contact',
-			cache: true
-		}
-	];
+    {
+      id: 'resume',
+      type: 'pdf',
+      src: assetsPath + '/docs/resume.pdf',
+      group: 'resume'
+    },
+
+    {
+      id: 'ContactController',
+      type: 'module',
+      src: 'controller/contact',
+      group: 'contact'
+    },
+
+    {
+      id: 'check',
+      type: 'image',
+      src: assetsPath + '/img/check.png',
+      group: 'contact'
+    },
+
+    {
+      id: 'mail',
+      type: 'audio',
+      src: assetsPath + '/audio/mail.mp3',
+      group: 'contact',
+      cache: true
+    }
+  ];
 
 });
