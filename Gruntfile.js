25575bd3a985316c423eca000dfaf2ba2c00e7fe
James <jamespdlynn@gmail.com>
2022-05-31 15:37:21 -0500

    added new fav icon build step

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,179 +3,247 @@
  * @author James lynn
  */
 module.exports = function (grunt) {
-	'use strict';
+  'use strict';
 
-	require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt);
 
-	// Grunt Config
-	grunt.initConfig({
+  // Grunt Config
+  grunt.initConfig({
 
-		jshint: {
-			target: {
-				options: {
-					jshintrc: '.jshintrc'
-				},
-				files: {
-					src: [
-						'src/js/scripts/**/*.js',
-						'test/js/spec/**/*.js'
-					]
-				}
-			}
-		},
+    jshint: {
+      target: {
+        options: {
+          jshintrc: '.jshintrc'
+        },
+        files: {
+          src: [
+            'src/js/scripts/**/*.js',
+            'test/js/spec/**/*.js'
+          ]
+        }
+      }
+    },
 
-		karma : {
-			target : {
-				configFile : 'test/karma.conf.js'
-			}
-		},
+    karma: {
+      target: {
+        configFile: 'test/karma.conf.js'
+      }
+    },
 
-		clean: {
-			build: ['build'],
-			dist : ['dist']
-		},
+    clean: {
+      build: ['build'],
+      dist: ['dist']
+    },
 
-		copy: {
-			build : {files: [
-				{
-					cwd: 'src/',
-					expand: true,
-					dest: 'build/',
-					src: '**/*'
-				}
-			]},
+    copy: {
+      build: {
+        files: [
+          {
+            cwd: 'src/',
+            expand: true,
+            dest: 'build/',
+            src: '**/*'
+          }
+        ]
+      },
 
-			dist : {files: [
-				{
-					cwd: 'build/assets',
-					expand: true,
-					dest: 'dist/assets',
-					src: '**/*'
-				},
-				{
-					cwd: 'build/js/lib',
-					expand: true,
-					dest: 'dist/js/lib',
-					src: ['*.min.js','require.js']
-				},
-				{
-					src: 'build/favicon.ico',
-					dest: 'dist/favicon.ico'
-				}
-			]}
-		},
+      dist: {
+        files: [
+          {
+            cwd: 'build/assets',
+            expand: true,
+            dest: 'dist/assets',
+            src: '**/*'
+          },
+          {
+            cwd: 'build/js/lib',
+            expand: true,
+            dest: 'dist/js/lib',
+            src: ['*.min.js', 'require.js']
+          }
+        ]
+      }
+    },
 
-		less: {
-			target : {
-				options: {
-					compress : true
-					/*modifyVars: {
-						imgPath: config.cdnUrl
-					}*/
-				},
-				files : {'dist/css/style.css' : 'build/css/style.less'}
-			}
+    less: {
+      target: {
+        options: {
+          compress: true
+          /*modifyVars: {
+            imgPath: config.cdnUrl
+          }*/
+        },
+        files: {'dist/css/style.css': 'build/css/style.less'}
+      }
 
-		},
+    },
 
-		htmlmin: {
-			target: {
-				options: {
-					removeComments: true,
-					collapseWhitespace: true,
-					collapseBooleanAttributes: true,
-					removeAttributeQuotes: true,
-					removeRedundantAttributes: true,
-					removeEmptyAttributes: true,
-					removeEmptyElements: false
-				},
-				files: [
-					{'dist/index.html': 'build/index.html'},
-					{
-						cwd: 'build/templates/',
-						expand: true,
-						dest: 'dist/templates/',
-						src: ['**/*.html']
-					}
-				]
-			}
-		},
+    htmlmin: {
+      target: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true,
+          collapseBooleanAttributes: true,
+          removeAttributeQuotes: true,
+          removeRedundantAttributes: true,
+          removeEmptyAttributes: true,
+          removeEmptyElements: false
+        },
+        files: [
+          {'dist/index.html': 'build/index.html'},
+          {
+            cwd: 'build/templates/',
+            expand: true,
+            dest: 'dist/templates/',
+            src: ['**/*.html']
+          }
+        ]
+      }
+    },
 
-		ngAnnotate : {
-			options : {
-				singleQuotes : true
-			},
+    ngAnnotate: {
+      options: {
+        singleQuotes: true
+      },
 
-			target : {
-				files: [{
-					expand : true,
-					cwd: 'build/js/scripts',
-					dest: 'build/js/scripts',
-					src: '**/*.js'
-				}]
-			}
-		},
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'build/js/scripts',
+          dest: 'build/js/scripts',
+          src: '**/*.js'
+        }]
+      }
+    },
 
-		uglify: {
-			target: {
-				files: [{
-					expand: true,
-					cwd: 'build/js/scripts',
-					src: '**/*.js',
-					dest: 'dist/js/scripts'
-				},
-				{'dist/js/main.js' : 'build/js/main.js'}]
-			}
-		},
+    uglify: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'build/js/scripts',
+          src: '**/*.js',
+          dest: 'dist/js/scripts'
+        },
+          {'dist/js/main.js': 'build/js/main.js'}]
+      }
+    },
 
-		imagemin : {
-			target : {
-				files: [{
-					expand: true,
-					cwd: 'build/assets/img/',
-					src: ['**/*.{png,jpg,gif}'],
-					dest: 'build/assets/img/'
-				}]
-			}
-		},
+    imagemin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'build/assets/img/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'build/assets/img/'
+        }]
+      }
+    },
 
-		execute : {
-			target : {
-				options: {
-					args: ['build', 'dist']
-				},
-				src: ['sitemap.js']
-			}
-		}
+    execute: {
+      target: {
+        options: {
+          args: ['build', 'dist']
+        },
+        src: ['sitemap.js']
+      }
+    },
 
-	});
+    realFavicon: {
+      favicons: {
+        src: 'build/favicon.png',
+        dest: 'dist',
+        options: {
+          iconsPath: '/',
+          html: ['dist/index.html'],
+          design: {
+            ios: {
+              pictureAspect: 'backgroundAndMargin',
+              backgroundColor: '#4c96de',
+              margin: '0%',
+              assets: {
+                ios6AndPriorIcons: false,
+                ios7AndLaterIcons: false,
+                precomposedIcons: false,
+                declareOnlyDefaultIcon: true
+              }
+            },
+            desktopBrowser: {
+              design: 'raw'
+            },
+            windows: {
+              pictureAspect: 'noChange',
+              backgroundColor: '#4c96de',
+              onConflict: 'override',
+              assets: {
+                windows80Ie10Tile: false,
+                windows10Ie11EdgeTiles: {
+                  small: false,
+                  medium: true,
+                  big: false,
+                  rectangle: false
+                }
+              }
+            },
+            androidChrome: {
+              pictureAspect: 'shadow',
+              themeColor: '#4c96de',
+              manifest: {
+                name: 'James Lynn',
+                display: 'standalone',
+                orientation: 'notSet',
+                onConflict: 'override',
+                declared: true
+              },
+              assets: {
+                legacyIcon: false,
+                lowResolutionIcons: false
+              }
+            },
+            safariPinnedTab: {
+              pictureAspect: 'silhouette',
+              themeColor: '#4c96de'
+            }
+          },
+          settings: {
+            scalingAlgorithm: 'Mitchell',
+            errorOnImageTooSmall: false,
+            readmeFile: false,
+            htmlCodeFile: false,
+            usePathAsIs: false
+          }
+        }
+      }
+    }
+  });
 
-	//Run jshint and karma unit tests on scripts
-	grunt.registerTask('test', ['jshint', 'karma']);
+  //Run jshint and karma unit tests on scripts
+  grunt.registerTask('test', ['jshint', 'karma']);
 
-	//Deploy a production build to the 'dist' directory
-	grunt.registerTask('deploy', [
+  //Deploy a production build to the 'dist' directory
+  grunt.registerTask('deploy', [
 
-		'clean', //Delete both build and dist directories
+    'clean', //Delete both build and dist directories
 
-		'copy:build', //Copy source files to temporary build directory
+    'copy:build', //Copy source files to temporary build directory
 
-		'htmlmin', //Minify html pages and templates
+    'htmlmin', //Minify html pages and templates
 
-		'less', //Compile and compress LESS files to css
+    'less', //Compile and compress LESS files to css
 
-		'ngAnnotate', //Annotate AngularJS injections so they can be minified
+    'ngAnnotate', //Annotate AngularJS injections so they can be minified
 
-		'uglify', //Minify Javascript
+    'uglify', //Minify Javascript
 
-		'imagemin', //Compress image assets
+    'realFavicon', //create fav icons
 
-		'copy:dist', //Copy remaining files to dist directory
+    'imagemin', //Compress image assets
 
-		'execute', //Create sitemap xml for crawlers
+    'copy:dist', //Copy remaining files to dist directory
 
-		'clean:build' //Delete temporary build directory
+    'execute', //Create sitemap xml for crawlers
 
-	]);
+    'clean:build' //Delete temporary build directory
+
+  ]);
 
 };
--- README.md
+++ README.md
@@ -2,15 +2,19 @@
 
 >This is the source code for my online portfolio which can be found at [jameslynn.io](http://jameslynn.io/).
 
->The site is built mainly using the AngularJS clientside framework. 
+>The site is built mainly using the AngularJS clientside framework.
  The AngularAMD library is included to help adapt Angular for use with the RequireJS AMD module loader.
- 
-### Run app locally
+
+### Install Dependencies
   * npm install
+
+### Run app locally
   * npm start
 
-### Deploy and run Production Build
+### Run Tests
+  * npm test
 
+### Deploy (with Grunt) and run production build
   * npm install --prod=false
   * npm run deploy
   * NODE_ENV=production npm start
